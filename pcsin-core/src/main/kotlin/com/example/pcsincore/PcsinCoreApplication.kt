7f94ee165b6f78deeb920b71a8f0b39e9aa5a5b6
koldin108902 <alex108902@naver.com>
2023-10-13 03:30:19 +0900

    init

---
+++ pcsin-core/src/main/kotlin/com/example/pcsincore/PcsinCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pcsincore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PcsinCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<PcsinCoreApplication>(*args)
+}
---
+++ pcsin-core/src/test/kotlin/com/example/pcsincore/PcsinCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pcsincore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PcsinCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
