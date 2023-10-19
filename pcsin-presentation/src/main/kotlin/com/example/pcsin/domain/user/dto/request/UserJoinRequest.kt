69baa0ce9bfc4de8b03f243288b0369026d745fb
koldin108902 <alex108902@naver.com>
2023-10-19 15:47:52 +0900

    Fix: joinReq name field 추가

--- pcsin-presentation/src/main/kotlin/com/example/pcsin/domain/user/dto/request/UserJoinRequest.kt
+++ pcsin-presentation/src/main/kotlin/com/example/pcsin/domain/user/dto/request/UserJoinRequest.kt
@@ -6,12 +6,17 @@ import jakarta.validation.constraints.NotEmpty
 
 data class UserJoinRequest(
     @field:NotEmpty
-    @field:Min(3, message = "정상적이지 않은 값입니다.")
+    @field:Min(3, message = "정상적이지 않은 값 입니다.")
     val email: String,
     @field:NotEmpty
     @field:Min(8, message = "비밀번호는 8자 이상이어야 합니다.")
     val password: String,
+
+    @field:NotEmpty
+    val name: String
 ) {
     fun toDomain(): User =
-        User(email=this.email, password = this.password)
+        User(email=this.email,
+            password = this.password,
+            name = this.name)
 }
